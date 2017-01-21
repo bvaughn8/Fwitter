89647396a6a0187467e299aff92284671d1891b8
Tiffany Xu <tiffanyxu@lawn-128-61-0-142.lawn.gatech.edu>
2017-01-21 14:36:09 -0500

    Updated with feedpage

--- Fwitter.py
+++ Fwitter.py
@@ -1,22 +1,44 @@
-from flask import Flask
+from flask import Flask, render_template, url_for, request
+from flask_login import LoginManager, UserMixin, login_required, current_user
 app = Flask(__name__)
+login_manager = LoginManager()
+login_manager.init_app(app)
 
+class Post:
+    def __init__(self, username, content):
+        self.username = username
+        self.content = content
+postList = []
+user_dataBase = {"txtxxu":"tiffanyxu816","bvaughn":"123","adam":"234","chris":"234"}
+
+class User(UserMixin):
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+    
 @app.route('/login', methods=['GET', 'POST']) #log-in page 
 def login():
-    return 
+    return "login screen"
 
 
-@app.route('/')  #feedpage after login
+@app.route('/feed', methods=["POST"])  #feedpage after login
+@login_required
 def index():
-    return "FWITTER"
-
-
+    if request.method == "POST":
+        content = request.form["tweet"] #request.form is a dictionary
+        username = current_user.username
+        post = Post(username, content)
+        postList.append(post)
+    #return render_template() #enter html
+    return postList
+    
 @app.route('/user/<username>') #user page 
 def user(username):
     return "My name is {}".format(username)
 
-
-
+@app.route('/logout')
+def logout():
+    return redirect(url_for('/login'))
 
 if __name__ == "__main__":
     app.run()
